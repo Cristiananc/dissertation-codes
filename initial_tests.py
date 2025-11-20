43c1895f84109e8198a901288219cf9c448e3c1e
Cristiana <cristiananc@hotmail.com>
2025-11-20 19:24:50 -0300

    initial test to check if codes are running

---
+++ initial_tests.py
@@ -0,0 +1,32 @@
+# Importing the required modules and packages
+import networkx as nx
+from epidemic_simulation.sir_simulation import fast_SIR
+from epidemic_simulation.helpers import *
+from sample.helpers import *
+from sample.sampling_tress import feasible_tree, sampling_trees
+
+#Creating our initial example of a random network
+n = 10
+p = 5/(n - 1)
+G = nx.erdos_renyi_graph(n, p)
+
+beta = 0.4
+fast_SIR(G, [0], beta, n)
+
+#Selecting a fraction of nodes that will not be observed.
+p_excluded = 0.2
+excluded, infected_nodes = excludeInfTime(G, p_excluded)
+print(nx.get_node_attributes(G, "inf_time"))
+
+#Deleting nodes that we known were not infected from the graph.
+delete_susceptibles(G)
+
+T_initial = feasible_tree(G, infected_nodes)
+print(T_initial)
+
+#samplings_number = 2
+#samplings = sampling_trees(G, T_initial, samplings_number, infected_nodes)
+
+#Proportion of nodes found
+#nodes_prop = nodes_proportion(G, samplings, samplings_number)
+#print(nodes_prop)
