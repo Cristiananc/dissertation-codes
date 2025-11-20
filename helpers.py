85cf3a90a0ec88d17acb69bc1c6994c379061801
Cristiana <cristiananc@hotmail.com>
2025-11-20 12:15:47 -0300

    adding file with auxilary functions

---
+++ helpers.py
@@ -0,0 +1,23 @@
+#This file contains the auxiliary functions to perform the epidemic simulations 
+import math
+import numpy as np
+import networkx as nx
+
+#This function deletes the infection time information from a fraction p of nodes
+# INPUT: A graph G in networkx format and a decimal p which stands for the
+# desired fraction to be deleted.
+def excludeInfTime(G, p):
+    N = round(p*len(G.nodes))
+
+    #We remove the node 0 from the possible removed nodes.
+    excluded_nodes = np.random.choice(list(G.nodes)[1:], N, replace = False)
+
+    for node in excluded_nodes:
+        G.nodes[node]['inf_time'] = math.inf
+        G.nodes[node]['status'] = ''
+
+    obs_infected_nodes = [k for k,v in nx.get_node_attributes(G, 'status').items() if v.startswith('r')]
+
+
+    return excluded_nodes, obs_infected_nodes
+
