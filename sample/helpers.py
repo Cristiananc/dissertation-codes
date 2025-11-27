5917aaba8ce0061ee899e8054a818a66dd5996a8
Cristiana <cristiananc@hotmail.com>
2025-11-27 16:34:02 -0300

    check feasibility of a tree

--- sample/helpers.py
+++ sample/helpers.py
@@ -1,9 +1,5 @@
-import networkx as nx
-from functools import reduce 
-#from operator import mul
-import math
 
-def nodes_proportion(G, samplings, samplings_number):
+def nodes_proportion(G, samplings):
     """
     Returns the proportion of each node in a sampling.
     """
@@ -13,62 +9,30 @@ def nodes_proportion(G, samplings, samplings_number):
             is_present = any(node in sublist for sublist in lis)
             nodes_prop[node] += is_present
 
+    n = len(samplings)
+    
     for node in nodes_prop:
-        nodes_prop[node] = nodes_prop[node]/(samplings_number + 1)
-
+        nodes_prop[node] = nodes_prop[node]/n
     return nodes_prop
 
-def prob_tree_log(G, T, beta):
+def check_feasibility_graphs(G, T, infected_nodes):
     """
-    Returns the log probability of a transmission tree.
+    Checks if a given Tree embeeded in a graph is feasible and returns a boolean 
+    accordingly.
     """
+   
+    for node in infected_nodes:
 
-    succes_events =reduce(lambda count, l: count + len(l) - 1, T, 0)
-    total_events = G.degree[0]
-
-    for lis in T[1:]:
-        for node in lis[0:-1]:
-            total_events += G.degree[node] - 1
+        if G.nodes[node]['inf_time'] == T.nodes[node]['inf_time']:
+            continue
+        else:
+            print("The tree provided is not a feasible tree!")
+            return False
     
-    failed_events = total_events - succes_events
+    return True
 
-    prob_log = math.log(beta**(succes_events)*(1 - beta)**failed_events)
-
-    return prob_log
-
-
-def prob_path_log(G, path):
+def check_feasibility_tree(G, T):
     """
-    This is based on the Degree-Biased Random Walk
-    Returns the log probability of a path according to the degree of the nodes.
-    It should return Inf if a path does not exist.
-    Disclaimer: This code was refactored with AI assistance.
+    Checks if a given tree in list of lists format is feasible.
     """
-    log_prob = 0
-
-    if len(path) < 2:
-        return log_prob
-  
-    #Iterating using a slide window
-    for i in range(len(path) - 1):
-      u = path[i]
-      v = path[i + 1]
-
-    #Check if nodes exist in the graph:
-    if u not in G or v not in G:
-      return math.inf
-
-    #Check if edge exists in the graph
-    if u not in G[v]:
-      return math.inf
-      
-    neighbors = list(G.neighbors(u))
-    norm_factor = sum([G.degree[i] for i in neighbors]) 
-
-    if norm_factor == 0:
-      return math.inf 
-
-    numerator = G.degree(v)
-    log_prob += math.log(numerator) - math.log(norm_factor) 
-      
-    return log_prob
+    return None
