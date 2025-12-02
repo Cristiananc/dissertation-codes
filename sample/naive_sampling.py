0b521b298599aab0c830745b94990cadac052e67
Cristiana <cristiananc@hotmail.com>
2025-12-02 11:10:36 -0300

    addition of infected nodes list

--- sample/naive_sampling.py
+++ sample/naive_sampling.py
@@ -2,27 +2,29 @@
 This file performs a naive approach to the problem. 
 """
 
+import numpy as np
+import networkx as nx
 import copy
+import math
 from .helpers import check_feasibility_graphs
-from .sampling_trees import feasible_tree
 from epidemic_simulation.sir_simulation import fast_SIR
 
-def naive_sampling(G, sampling_number, infected_nodes, initial_infecteds, p, n):
+def naive_sampling(G, sampling_number, observed_nodes, initial_infecteds):
 
     samplings = []
     G_mutable = copy.deepcopy(G)
 
     while len(samplings) < sampling_number:
 
+        p = np.random.uniform()
+
         #In place modification of G
         fast_SIR(G_mutable, initial_infecteds, p)
 
-        if check_feasibility_graphs(G, G_mutable, infected_nodes):
-            current_T = feasible_tree(G_mutable, infected_nodes)
+        if check_feasibility_graphs(G, G_mutable, observed_nodes):
+            all_nodes = nx.get_node_attributes(G_mutable, "inf_time")
+            nodes_infected = [node for node, inf_time in all_nodes.items() if inf_time < math.inf]
 
-            while any(None in sublist for sublist in current_T):
-                current_T = feasible_tree(G_mutable, infected_nodes)
-            
-            samplings.append(current_T)
+            samplings.append(nodes_infected)
 
     return samplings
